3478d5463f8c8ff651c098a2b952383fe2066b21
MousElPsyCongroo <mtapha.tall@gmail.com>
2022-10-10 14:11:44 +0200

    Create FileStorage class with private class attributes

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python3
+"Serializes and deserializes JSON instances to/from files"
+
+
+class FileStorage:
+    """
+    Serializes and deserializes JSON instances to/from files
+
+    Attributes:
+        Class attributes:
+            __file_path: string - path to the JSON file
+            __objects: dictionary - stores all objects by <class name>.id
+    """
+    __file_path = ""
+    __objects = {}
