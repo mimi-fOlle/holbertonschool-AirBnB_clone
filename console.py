7fdcce7a702e5ce699414a068afd8d4e3298ffc5
MousElPsyCongroo <mtapha.tall@gmail.com>
2022-10-12 11:23:44 +0200

    Add console destroy command

--- console.py
+++ console.py
@@ -24,36 +24,64 @@ class HBNBCommand(cmd.Cmd):
     def emptyline(self):
         pass
 
+    def precmd(self, line):
+        "Handles command errors"
+        if len(line) > 0:
+            args = line.split()
+            command = args[0]
+            if command == "create":
+                if len(args) < 2:
+                    print("** class name missing **")
+                    return ""
+                elif args[1] != "BaseModel":
+                    print("** class doesn't exist **")
+                    return ""
+                return line
+
+            elif command in ("show", "destroy"):
+                if len(args) < 2:
+                    print("** class name missing **")
+                    return ""
+                elif args[1] != "BaseModel":
+                    print("** class doesn't exist **")
+                    return ""
+                elif len(args) < 3:
+                    print("** instance id missing **")
+                    return ""
+                return line
+        return line
+
     def do_create(self, arg):
         "Creates a new instance of BaseModel and saves it to the JSON file"
-        if not arg:
-            print("** class name missing **")
-        elif arg != "BaseModel":
-            print("** class doesn't exist **")
-        else:
-            inst = BaseModel()
-            inst.save()
-            print(inst.id)
+        inst = BaseModel()
+        inst.save()
+        print(inst.id)
 
     def do_show(self, arg):
         ("Prints the str representation of an instance based on class name "
          "and id")
         args = arg.split()
-        if len(args) < 1:
-            print("** class name missing **")
-        elif len(args) < 2:
-            print("** instance id missing **")
-        elif args[0] != "BaseModel":
-            print("** class doesn't exist **")
-        else:
-            id = args[1]
-            insts = models.storage.all()
-            key = f"BaseModel.{id}"
+        id = args[1]
+        insts = models.storage.all()
+        key = f"BaseModel.{id}"
 
-            if key in insts:
-                print(insts[key])
-            else:
-                print("** no instance found **")
+        if key in insts:
+            print(insts[key])
+        else:
+            print("** no instance found **")
+
+    def do_destroy(self, arg):
+        "Deletes an instance based on class name and id"
+        args = arg.split()
+        id = args[1]
+        insts = models.storage.all()
+        key = f"BaseModel.{id}"
+
+        if key in insts:
+            insts.pop(key)
+            models.storage.save()
+        else:
+            print("** no instance found **")
 
 
 if __name__ == '__main__':
